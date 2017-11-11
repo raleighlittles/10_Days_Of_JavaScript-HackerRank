df2d7b155292951d46416ddff4d2be4f203deea0
Raleigh Littles <raleighlittles@gmail.com>
2017-11-11 03:55:42 -0800

    Finished Day 0 and added Day 1 challenges, also restructured folders

---
+++ Day 1/Functions.js
@@ -0,0 +1,13 @@
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n)
+{
+    if (n < 2)
+        {
+            return 1;
+        }
+    
+    return n * factorial(n-1);
+}
---
+++ Day 1/Let and Const.js
@@ -0,0 +1,12 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    var r = parseInt(readLine());
+    // Print the area of the circle:
+    var area = PI * r * r;
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r;
+    console.log(perimeter);
+    
