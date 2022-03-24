83365a71b7a3624d3159c1b60217863678f0c957
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2022-03-24 15:49:00 +0530

    Day 9 30/30 solution

--- Day 9/binaryCalculator.js
+++ Day 9/binaryCalculator.js
@@ -1,33 +1,19 @@
+let res = document.getElementById("res");
+const reset = () => (res.innerHTML = "");
+const input = (a) => (res.innerHTML += a);
 
-// On clicking either 0 or 1, add the corresponding text to the result inner html
-// On clicking any operator, add that operator text to the result inner html
-// On clicking button clear, clear the results field
-// On clicking equals, evaluate the expression
-
-document.getElementById('btn0').addEventListener('click', function()
-{
-  document.getElementById('res').insertAdjacentHTML('beforeend', '0');
-});
-
-document.getElementById('btn1').addEventListener('click', function()
-{
-  document.getElementById('res').insertAdjacentHTML('beforeend', '1');
-});
-
-function operatorAction(eve)
-{
-    var btn = eve.target;
-    // Add the inner html for the operator buttons into the results field
-    document.getElementById('res').insertAdjacentHTML('beforeend', btn.innerHTML);
+function render() {
+  let code = res.innerHTML;
+  if (!code) return;
+  const matches = code.match(/[0-1]+/g);
+  if (matches) {
+    for (const match of matches)
+      code = code.replace(match, parseInt(Number(match), 2));
+  }
+  try {
+    eval(`code=(${code})`);
+  } catch (e) {
+    console.error(e);
+  }
+  res.innerHTML = Math.round(code).toString(2);
 }
-
-document.getElementById('btnSum').onclick = operatorAction;
-document.getElementById('btnMul').onclick = operatorAction;
-document.getElementById('btnDiv').onclick = operatorAction;
-document.getElementById('btnSub').onclick = operatorAction;
-
-document.getElementById('btnClr').onclick = function()
-{
-    (document.getElementById('res').innerHTML) = "";
-};
-
