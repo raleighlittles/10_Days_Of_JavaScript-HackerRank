ff441d80f6b0b54da9dbd25ddede710134bfdca6
Rahmat <oktrifianto@gmail.com>
2022-04-17 23:00:16 +0700

    Update: unique sorted array using `Set()` method

--- Day 3/Arrays.js
+++ Day 3/Arrays.js
@@ -5,10 +5,7 @@
 **/
 function getSecondLargest(nums) {
     // Complete the function
-    var sorted_array = nums.sort(function (a,b) {return a - b;});
-    var unique_sorted_array = sorted_array.filter(function(elem, index, self) {
-        return index === self.indexOf(elem);
-    })
-
+    const sorted_array          = nums.sort((a,b) => a - b );
+    const unique_sorted_array   = [...new Set(sorted_array)];
     return unique_sorted_array[unique_sorted_array.length - 2];
 }
